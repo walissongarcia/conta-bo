4e6e77eb3e6a1054c391edf7c6cda23b150d5a17
walissongarcia <128553822+walissongarcia@users.noreply.github.com>
2023-05-12 20:45:34 -0300

    inicio da conta

---
+++ conta.java
@@ -0,0 +1,34 @@
+public class conta {
+
+    private int numero;
+
+    private string nomeagencia;
+
+    private int numueroagencia;
+
+    private string nomebanco;
+
+    private int numerobanco;
+
+    private string tipoconta;
+
+    private float saldo;
+
+    public void abrir() {
+    }
+
+    public void encerrar() {
+    }
+
+    public void consultar() {
+    }
+
+    public void saldo() {
+    }
+
+    public void creditar() {
+    }
+
+    public void debitar() {
+    }
+}
